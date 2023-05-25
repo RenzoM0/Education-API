58c07f0a4bbd2a1019b1e8b572988851f8925c79
Renzo Macnack <lma.macnack@outlook.com>
2023-05-25 17:45:05 +0200

    Chat history works
    Chat bot remembers conversation
    Math instructions added

--- aiapi.py
+++ aiapi.py
@@ -2,17 +2,26 @@
 import openai
 import config
 #API key for Chat GPT server (look at config.py)      
-
 openai.api_key = config.DevelopmentConfig.OPENAI_KEY
-#TODO: more prices
-testresponse = """ You are a pirate"""
-instructionscourse1 = """ You are a teacher.
-    When a math question is asked explain step by step how to solve the math problem.
-    Suggest a similar question to solve themselves.
+
+chat_history = []  # Initialize an empty chat history
+
+def_instruction = """"""
+instructionscourse1 = """ 
+ As an AI assistant, your goal is to help students understand math concepts clearly. Provide concise explanations and use visuals if necessary.
+ Encourage students to ask questions and actively engage in problem-solving. Foster a supportive learning environment.
+ Emphasize the importance of practice and repetition to reinforce math skills. Encourage students to work on additional exercises.
+ When explaining complex math concepts, break them down into simpler terms and relate them to real-life examples whenever possible.
+ Provide alternative approaches to problem-solving and encourage students to think creatively.
+ If a student is struggling, offer hints or prompts to guide them in the right direction rather than immediately providing the solution.
+ Highlight the practical applications of math in various fields, showcasing its relevance and motivating students to learn.
+ Celebrate students' progress and achievements to boost their confidence and foster a positive attitude towards math.
+ If the student goes of topic kindly ask them to keep it on topic.
     """
 i_course1_quiz = """"""
 instructionscourse2 = """ You are a Research teacher, you help students to learn about doing research. 
     If the student asks you questions not about research, ask them kindly to keep it on topic."""
+
 # Switching between Chat GPT system instructions
 def switch(number):
     if number == 1:
@@ -22,24 +31,37 @@ def switch(number):
     elif number == 3:
         return i_course1_quiz
     else:
-        return testresponse
-# Dependening on switch(number) ask response from Chat GPT server.
-def generateChatResponse(prompt, number):
-    content = switch(number)
-                 
-    messages = []
-    messages.append({"role": "system", "content": content})
-    
-    question = {}
-    question['role'] = 'user'
-    question['content'] = prompt
-    messages.append(question)
-    
-    response = openai.ChatCompletion.create(model="gpt-3.5-turbo",messages=messages)
-    
-    try:
-        answer = response['choices'][0]['message']['content'].replace('\n', '<br>')
-    except:
-        answer = 'Oops you beat the AI, try a different question, if the problem persists, please contact the developer.'
-    
+        return def_instruction
+
+def generateChat(prompt, system_instructions):
+    global chat_history
+
+    # Add the user prompt and system instructions to the chat history
+    chat_history.append({
+        'role': 'system',
+        'content': switch(system_instructions)
+    })
+    chat_history.append({
+        'role': 'user',
+        'content': prompt
+    })
+
+    # Build the messages list with the chat history
+    messages = [{'role': 'system', 'content': message['content']} for message in chat_history]
+
+    # Make API call
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages
+    )
+
+    # Extract assistant's response from API response
+    answer = response.choices[0].message['content'].replace('\n', '<br>')
+
+    # Add the assistant's response to the chat history
+    chat_history.append({
+        'role': 'assistant',
+        'content': answer
+    })
+
     return answer
--- views.py
+++ views.py
@@ -7,17 +7,6 @@ views = Blueprint(__name__, "views")
 @views.route("/")
 def home():
     return render_template("home.html")
-# url: /views/chat for chat interface(test)
-@views.route("/chat", methods=['POST', 'GET'])
-def chat():
-    if request.method == 'POST':
-        prompt = request.form['prompt']
-        
-        res = {}
-        res['answer'] = aiapi.generateChatResponse(prompt, 0)
-        return jsonify(res), 200
-        
-    return render_template("c_interface.html")
 # url: /views/course1chat for chat course 1
 @views.route("/course1chat", methods=['POST', 'GET'])
 def course1chat():
@@ -25,7 +14,7 @@ def course1chat():
         prompt = request.form['prompt']
         
         res = {}
-        res['answer'] = aiapi.generateChatResponse(prompt, 1)
+        res['answer'] = aiapi.generateChat(prompt, 1)
         return jsonify(res), 200
         
     return render_template("c_interface_course1.html")
