45e8c3f1faa2d66ee730c7b18ca7fda67f0eb76a
Renzo Macnack <lma.macnack@outlook.com>
2023-06-02 11:48:53 +0200

    Personality settings page
    some page edits

--- website/views.py
+++ website/views.py
@@ -12,6 +12,11 @@ views = Blueprint('views', __name__)
 def home():
     return render_template("home.html", user=current_user)
 
+@views.route('/personality')
+@login_required
+def personality():
+    return render_template("personality.html", user=current_user)
+
 @views.route('/chathistory')
 @login_required
 def chathistory():
@@ -44,9 +49,20 @@ def course1chat():
 @views.route("/course2chat", methods=['POST', 'GET'])
 def course2chat():
     if request.method == 'POST':
-        prompt = request.form['prompt']
-        res = {}
-        res['answer'] = aiapi.generateChat(prompt, 2)
-        return jsonify(res), 200
+        if 'commit' in request.form:  # Check if the 'commit' button was pressed
+            chatdata = aiapi.get_chat_history()
+            # Convert chatdata to a JSON string
+            chatdata_json = json.dumps(chatdata)
+        
+            # Create a new Chat object and save it to the database
+            new_chat = Chat(course='Research', chatlog=chatdata_json, student=current_user.id)
+            db.session.add(new_chat)
+            db.session.commit()
+            flash('Chat history saved!', category='success')
+        else:    
+            prompt = request.form['prompt']    
+            res = {}
+            res['answer'] = aiapi.generateChat(prompt, 1) 
+            return jsonify(res), 200
         
     return render_template("c_interface_course2.html", user=current_user)
